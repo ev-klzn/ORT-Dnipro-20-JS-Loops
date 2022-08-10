80619a01e71502dfd69c96e6f7b7f1f8d5a8f515
ev-klzn <zhekan89@gmail.com>
2022-08-10 11:02:31 +0300

    1

--- assets/js/1.js
+++ assets/js/1.js
@@ -1,10 +1,18 @@
-num = +prompt(`Введите значение от 1 до 100`);
-for(let i = 2; i < num; i++); 
-    if (num % i === 0)
+let num = +prompt('Введите число');
+console.log(`введенно число ${num}`);
+let i;
+let isFail = true;
+for(let i = 2; i < num; i++){
+    if (num % i === 0){
+        isFail = false;
+        break;
+    }
+}
 
-
-console.log()
-
-
-
-
+if(isFail){
+    alert(`число ${num} прострое`);
+    console.log(`число ${num} прострое`);
+}else{
+    alert(`число ${num} непрострое`)
+    console.log(`число ${num} непрострое`);
+}
